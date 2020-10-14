e23e5d3ad2ee298883d002942fba3966aa7e1b46
Garrett <gahe0404@colorado.edu>
2020-10-14 00:17:38 -0600

    adding employee and its subclasses

--- restaurant/Customer.java
+++ restaurant/Customer.java
@@ -3,5 +3,7 @@ package restaurant;
 public abstract class Customer {
     protected String name;
     public String getName() { return name; }
+    public FoodItem[] rollOrder;
+    public double totalOrderCost = 0.0;
 
 }
---
+++ restaurant/Employee.java
@@ -0,0 +1,28 @@
+package restaurant;
+
+public abstract class Employee {
+    protected double dailySpringSales, dailyEggSales, dailyPastrySales, dailySausageSales, dailyJellySales;
+    protected int dailyTotalSpringOrders, dailyTotalEggOrders, dailyTotalPastryOrders, dailyTotalSausageOrders, dailyTotalJellyOrders;
+
+    protected double dailyCasualSales, dailyBusinessSales, dailyCateringSales;
+    protected int casualRollOutages,businessRollOutages, cateringRollOutages;
+    protected double dailyTotalSales;
+
+    Boolean earlyClosure;
+
+    protected double allSpringSales, allEggSales, allPastrySales, allSausageSales, allJellySales;
+    protected double lifetimeTotalSales;
+
+    public static Customer currentCustomer;
+
+    Employee(){
+        dailySpringSales = 0; dailyEggSales = 0; dailyPastrySales = 0; dailySausageSales = 0; dailyJellySales = 0;
+        dailyTotalSpringOrders = 0; dailyTotalEggOrders = 0; dailyTotalPastryOrders = 0; dailyTotalSausageOrders = 0; dailyTotalJellyOrders = 0;
+        allSpringSales = 0; allEggSales = 0; allPastrySales = 0; allSausageSales = 0; allJellySales = 0;
+        dailyCasualSales = 0; dailyBusinessSales = 0; dailyCateringSales = 0;
+        casualRollOutages = 0; businessRollOutages = 0; cateringRollOutages = 0;
+        dailyTotalSales = 0;
+    }
+
+    public abstract void update();
+}
