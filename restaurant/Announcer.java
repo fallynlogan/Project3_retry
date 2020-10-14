ca72f78796fe2c86dc4fc533a9d01eeb89f87072
fallynlogan <falo7122@colorado.edu>
2020-10-14 01:51:25 -0600

    announcer -- wip

--- restaurant/Announcer.java
+++ restaurant/Announcer.java
@@ -9,20 +9,28 @@ public class Announcer extends Employee{
 
     @Override
     public void update(){
-        switch(currentCustomer.getCurrent){
+        switch(currentCustomer.getCurrentEvent()){
             case "arrival":
+                System.out.println(currentCustomer.getName() + " the " + currentCustomer.getClass().getSimpleName() + " Customer has arrived at the store!");
                 break;
             case "desired order":
                 break;
             case "actual order":
+                for(int i =0; i < currentCustomer.rollOrder.length; i++){
+                    System.out.println(currentCustomer.getName() +  " purchased a " + currentCustomer.rollOrder[i].getDescription());
+                }
                 break;
             case "business order failed":
+                System.out.println("Sorry " + currentCustomer.getName() + "! The order can not be fulfilled!");
                 break;
             case "cater order reduced":
+                System.out.println("Sorry " + currentCustomer.getName() + "! Your order cannot be completed entirely!");
                 break;
             case "sold out single roll":
+                //System.out.println("ATTENTION! Jelly Rolls are now currently unavailable. Sorry for the inconvenience!");
                 break;
             case "sold out all rolls":
+                System.out.println("THE STORE IS CURRENTLY SOLD OUT OF ROLLS AND IS CLOSING! See you tomorrow!");
                 break;
             case "leave store":
                 break;
